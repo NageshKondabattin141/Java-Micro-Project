f276a7ab51a76396ee5b7e459cec16ef24fbb213
NageshKondabattin141 <nageshkondabattin@gmail.com>
2023-10-28 15:48:44 +0530

    a

--- Homepage.java
+++ Homepage.java
@@ -24,16 +24,32 @@ class Homepage extends Frame implements ActionListener
         db=new Button("DELETE");
         ub=new Button("UPDATE");
         sb=new Button("SHOW");
-        
+        ab.setBounds(50, 250, 100,30 );
+        db.setBounds(50, 300, 100,30 );
+        ub.setBounds(50, 350, 100,30 );
+        sb.setBounds(50, 400, 100,30 );
+        add(ab);
+        add(db);
+        add(ub);
+        add(sb);
+        ab.addActionListener(this);
+        db.addActionListener(this);
+        ub.addActionListener(this);
+        sb.addActionListener(this);
         setVisible(true);
         
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource()==sb)
+        {
+            result=new TextArea(25, 10);
+            result.setBounds(280, 200, 450, 250);
+            add(result);
+        }
     }
 
     public static void main(String[] args) {
-    Homepage h=new Homepage();
+    Homepage h =new Homepage();
     }
 }
