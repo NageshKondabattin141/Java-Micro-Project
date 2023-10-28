08d038df3bd1c4d6e07055cf1f20a34936a7c181
NageshKondabattin141 <nageshkondabattin@gmail.com>
2023-10-28 14:56:19 +0530

    abc

--- Homepage.java
+++ Homepage.java
@@ -3,7 +3,9 @@ import java.awt.event.*;
 
 class Homepage extends Frame implements ActionListener 
 {
-        TextField c;
+        TextField search;
+        Button ab,db,ub,sb,searchB;
+        TextArea result;
     Homepage() {
         setSize(400, 400);
         setLayout(null);
@@ -12,7 +14,10 @@ class Homepage extends Frame implements ActionListener
                 System.exit(0);
             }
         });
-
+        search=new TextField(10);
+        search.setLocation(50, 50);
+        add(search);
+        
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -20,6 +25,6 @@ class Homepage extends Frame implements ActionListener
     }
 
     public static void main(String[] args) {
-
+    Homepage h=new Homepage();
     }
 }
